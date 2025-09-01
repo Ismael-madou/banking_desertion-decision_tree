3503245572edac26b94da5fc17a6d2b884b8c407
Ismael Madou Gagi <ismael.madou-gagi-grema@etudiant.univ-reims.fr>
2025-09-01 19:51:04 +0200

    Analyse univariée et bivariée des données de churn bancaire

--- src/univariate.py
+++ src/univariate.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.utils import resample
+
 
 # Remplace 'chemin/vers/ton_fichier.csv' par le chemin réel
 data = pd.read_csv('C:/Users/MGI/Desktop/banking_desertion-decision_tree/data/BankChurners.csv')
 
 #suppression des deux dernières colonnes
 data.drop(columns=data.columns[-2:], inplace=True)
+
+# Afficher le nombre de valeurs manquantes par colonne
+valeurs_manquantes = data.isnull().sum()
+print(valeurs_manquantes)
 
 data.head()
 data.describe()
@@ -41,6 +47,51 @@ variables_numeriques = data.select_dtypes(include=['int64', 'float64']).hist(bin
 plt.suptitle("Histogrammes des variables numériques", fontsize=16)
 plt.tight_layout()
 plt.show()
+
+
+df_majority = df[df.Attrition_Flag == 0]
+df_minority = df[df.Attrition_Flag == 1]
+
+df_majority_downsampled = resample(df_majority,
+                                   replace=False,
+                                   n_samples=len(df_minority),
+                                   random_state=42)
+
+df_balanced = pd.concat([df_majority_downsampled, df_minority])
+
+##############################---------------------Statistiques bivariées----------------------############################
+# Matrice de corrélation
+plt.figure(figsize=(12, 10))
+sns.heatmap(data.corr(numeric_only=True), annot=True, cmap='coolwarm', fmt='.2f')
+plt.title('Matrice de corrélation')
+plt.show()
+
+#boxplot pour la variable 'Customer_Age' selon le statut d'attrition
+sns.boxplot(x='Attrition_Flag', y='Customer_Age', data=data)
+plt.title("Âge selon le statut d'attrition")
+plt.show()
+
+# Afficher les tableaux croisés deux à deux
+for i in range(len(qualitatives)):
+    for j in range(i + 1, len(qualitatives)):
+        var1 = qualitatives[i]
+        var2 = qualitatives[j]
+        print(f"Tableau croisé : {var1} vs {var2}")
+        print(pd.crosstab(data[var1], data[var2]))
+        print("\n")
+
+
+# Calcul de la matrice de corrélation
+correlation_matrix = numeriques.corr()
+
+# Affichage avec une heatmap
+plt.figure(figsize=(14, 10))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=0.5)
+plt.title("Matrice de corrélation des variables numériques")
+plt.tight_layout()
+plt.show()
+
+
 
 
 
